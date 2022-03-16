de1c214883ec2bc4488fc873b3466a02368e8339
hyukjaekim <hjkim@precursor.co.kr>
2022-03-16 17:11:07 +0900

    login

--- index.js
+++ index.js
@@ -4,3 +4,4 @@
 var mydom = document.getElementById('myid');
 console.log(mydom);
 console.log('댓글 기능 추가');
+console.log('로그인 기능 로그 출력');
--- login.js
+++ login.js
@@ -1,2 +1,3 @@
 /* login을 담당하는 javascript file */
 console.log('loginjs');
+console.log('로그인 기능 로그 출력');
